c9706f562b6371525f0de796e4025cadafa65c1c
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-06-06 13:08:21 +0530

    added custom fonts to the app

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,19 @@ class MiApp extends StatelessWidget {
               Text(
                 'Pratik Rana',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               )
